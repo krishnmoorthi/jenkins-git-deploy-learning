8c0d75190bba523e5e8ab55c3873b7d84729f1f8
krishnmoorthi <t.krishnamoorthi@gmail.com>
2020-03-23 15:06:20 +0530

    jenkins autoconfig learning sample helloworld for testing

---
+++ python-automation-scripts/com/jcp/helloworld/build/build.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_101"
+export ANT_HOME="/c/Users/kthangav/apache-ant-1.10.3"
+export ANT_ARGS='-logger org.apache.tools.ant.listener.AnsiColorLogger'
+export PATH=$PATH:$ANT_HOME/bin
+ant build
---
+++ python-automation-scripts/com/jcp/helloworld/helloworld.py
@@ -0,0 +1,4 @@
+from datetime import date
+
+date_today = date.today()
+print ("Hello world %s", date_today.strftime("%Y-%m-%d %H:%M:%S")
